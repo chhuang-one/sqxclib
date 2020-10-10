ba86d6090283ba2ce5185058b09bbe5d8d18c2a7
C.H. Huang <plushuang.tw@gmail.com>
2020-10-11 00:35:55 +0800

    rearrange error code

--- sqxc/SqError.h
+++ sqxc/SqError.h
@@ -22,34 +22,32 @@ extern "C" {
 // error code type is uint16_t
 
 #define SQCODE_OK                    0
-#define SQCODE_WARNING               0x1000
-#define SQCODE_ERROR                 0x2000
-
-// warning - parser
-#define SQCODE_ENTRY_NOT_FOUND       3
-
-// warning - 
-#define SQCODE_REENTRY_DROPPED       55
 
 // error
-#define SQCODE_NOT_SUPPORT           4
-#define SQCODE_EXEC_ERROR            5
-#define SQCODE_STATIC_DATA           6
-#define SQCODE_OPEN_FAIL             7
+#define SQCODE_NOT_SUPPORT           1
 
-// error - parser
-#define SQCODE_TYPE_NOT_MATCH        14
-#define SQCODE_TYPE_NOT_SUPPORT      15
-#define SQCODE_TYPE_END_ERROR        16
-#define SQCODE_TOO_MANY_NESTED       17
-#define SQCODE_NO_ELEMENT_TYPE       18
+// parser - warning
+#define SQCODE_ENTRY_NOT_FOUND       11
+// parser - error
+#define SQCODE_TYPE_NOT_MATCH        21
+#define SQCODE_TYPE_NOT_SUPPORT      22
+#define SQCODE_TYPE_END_ERROR        23
+#define SQCODE_TOO_MANY_NESTED       24
+#define SQCODE_NO_ELEMENT_TYPE       25
 
-#define SQCODE_NO_MORE_DATA          25
-#define SQCODE_UNCOMPLETED_JSON      26
+// migration - warning
+#define SQCODE_REENTRY_DROPPED       31
+// migration - error
+#define SQCODE_REFERENCE_NOT_FOUND   41
+#define SQCODE_REFERENCE_EACH_OTHER  42
 
-// error - migration
-#define SQCODE_REFERENCE_NOT_FOUND   100
-#define SQCODE_REFERENCE_EACH_OTHER  101
+// SQL - error
+#define SQCODE_OPEN_FAIL             51
+#define SQCODE_EXEC_ERROR            52
+
+// JSON error
+#define SQCODE_UNCOMPLETED_JSON      61
+
 
 #ifdef __cplusplus
 }
