857f7009e74118432f47568c1acf9ead096615dd
C.H. Huang <plushuang.tw@gmail.com>
2020-12-26 08:01:14 +0800

    add Sqdb Database interface for MySQL. It doesn't implement any function.

---
+++ sqxc/SqdbMysql.c
@@ -0,0 +1,83 @@
+/*
+ *   Copyright (C) 2020 by C.H. Huang
+ *   plushuang.tw@gmail.com
+ *
+ * sqxc is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+#if defined(_MSC_VER)
+#define _CRT_SECURE_NO_WARNINGS
+#define snprintf	_snprintf
+#endif
+
+#include <stdio.h>      // snprintf
+
+#include <SqError.h>
+#include <SqdbMysql.h>
+#include <SqxcValue.h>
+#include <SqxcSql.h>
+
+
+static void sqdb_mysql_init(SqdbMysql* sqdb, SqdbConfigMysql* config);
+static void sqdb_mysql_final(SqdbMysql* sqdb);
+static int  sqdb_mysql_open(SqdbMysql* sqdb, const char* database_name);
+static int  sqdb_mysql_close(SqdbMysql* sqdb);
+static int  sqdb_mysql_exec(SqdbMysql* sqdb, const char* sql, Sqxc* xc, void* reserve);
+static int  sqdb_mysql_migrate(SqdbMysql* sqdb, SqSchema* schema, SqSchema* schema_next);
+
+static const SqdbInfo dbinfo = {
+	.size    = sizeof(SqdbMysql),
+	.product = SQDB_PRODUCT_MYSQL,
+	.column  = {
+		.has_boolean = 1,
+	 	.use_alter  = 1,
+		.use_modify = 0,
+	},
+
+	.init    = (void*)sqdb_mysql_init,
+	.final   = (void*)sqdb_mysql_final,
+    .open    = (void*)sqdb_mysql_open,
+    .close   = (void*)sqdb_mysql_close,
+    .exec    = (void*)sqdb_mysql_exec,
+    .migrate = (void*)sqdb_mysql_migrate,
+};
+
+const SqdbInfo* SQDB_INFO_MYSQL = &dbinfo;
+
+// ----------------------------------------------------------------------------
+// SqdbInfo
+
+static void sqdb_mysql_init(SqdbMysql* sqdb, SqdbConfigMysql* config_src)
+{
+}
+
+static void sqdb_mysql_final(SqdbMysql* sqdb)
+{
+}
+
+static int  sqdb_mysql_open(SqdbMysql* sqdb, const char* database_name)
+{
+	return SQCODE_OK;
+}
+
+static int  sqdb_mysql_close(SqdbMysql* sqdb)
+{
+	return SQCODE_OK;
+}
+
+static int  sqdb_mysql_migrate(SqdbMysql* sqdb, SqSchema* schema, SqSchema* schema_next)
+{
+	return SQCODE_OK;
+}
+
+static int  sqdb_mysql_exec(SqdbMysql* sqdb, const char* sql, Sqxc* xc, void* reserve)
+{
+	return SQCODE_OK;
+}
---
+++ sqxc/SqdbMysql.h
@@ -0,0 +1,106 @@
+/*
+ *   Copyright (C) 2020 by C.H. Huang
+ *   plushuang.tw@gmail.com
+ *
+ * sqxc is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+#ifndef SQDB_MYSQL_H
+#define SQDB_MYSQL_H
+
+#include <Sqdb.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct SqdbMysql           SqdbMysql;
+typedef struct SqdbConfigMysql     SqdbConfigMysql;
+
+extern const SqdbInfo*    SQDB_INFO_MYSQL;
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+/* ----------------------------------------------------------------------------
+    SqdbMysql - Sqdb for MySQL
+
+    Sqdb
+    |
+    `--- SqdbMysql
+
+   The correct way to derive Sqdb:  (conforming C++11 standard-layout)
+   1. Use Sq::DbMethod to inherit member function(method).
+   2. Use SQDB_MEMBERS to inherit member variable.
+   3. Add variable and non-virtual function in derived struct.
+   ** This can keep std::is_standard_layout<>::value == true
+ */
+
+#ifdef __cplusplus
+struct SqdbMysql : Sq::DbMethod          // <-- 1. inherit member function(method)
+#else
+struct SqdbMysql
+#endif
+{
+	SQDB_MEMBERS;                          // <-- 2. inherit member variable
+/*	// ------ Sqdb members ------
+	const SqdbInfo *info;
+ */
+
+	// ------ SqdbMysql members ------     // <-- 3. Add variable and non-virtual function in derived struct.
+};
+
+/* ----------------------------------------------------------------------------
+    SqdbConfigMysql - SqdbMysql use this configure
+
+    SqdbConfig
+    |
+    `--- SqdbConfigMysql
+ */
+struct SqdbConfigMysql
+{
+	SQDB_CONFIG_MEMBERS;
+/*	// ------ SqdbConfig members ------
+	unsigned int    product;
+	unsigned int    bit_field;
+ */
+
+	// ------ SqdbConfigMysql members ------
+};
+
+
+// ----------------------------------------------------------------------------
+// C++ namespace
+
+#ifdef __cplusplus
+
+namespace Sq {
+
+typedef struct SqdbConfigMysql    DbConfigMariadb;
+
+// conforming C++11 standard-layout
+// These are for directly use only. You can NOT derived it.
+struct DbMysql : SqdbMysql
+{
+	DbMysql(SqdbConfigMysql* config = NULL) {
+		this->info = SQDB_INFO_MYSQL;  SQDB_INFO_MYSQL->init((Sqdb*)this, (SqdbConfig*)config);
+	}
+	~DbMysql() {
+		SQDB_INFO_MYSQL->final((Sqdb*)this);
+	}
+};
+
+};  // namespace Sq
+
+#endif  // __cplusplus
+
+
+#endif  // SQDB_MYSQL_H
