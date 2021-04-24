1cd08e3e5597b2fc0a7a31afc400565fcf82d89f
C.H. Huang <plushuang.tw@gmail.com>
2021-04-25 05:01:08 +0800

    fix: When MySQL "ADD COLUMN", It doesn't recalculate size of C structure.

--- sqxc/Sqdb-migration.c
+++ sqxc/Sqdb-migration.c
@@ -126,6 +126,9 @@ int   sq_entry_update(SqEntry *entry, SqEntry *entry_src, SqDestroyFunc destroy_
 				// steal 'reentry_src' from 'entry_src->type'. TODO: NO_STEAL
 				if (entry_src->type->bit_field & SQB_DYNAMIC)
 					reentries_src->data[index] = NULL;
+				// calculate size if 'entry' is instance of SqTable
+				if (destroy_func == (SqDestroyFunc)sq_column_free)
+					sq_type_decide_size((SqType*)entry->type, (SqEntry*)reentry_src);
 			}
 #if DEBUG
 			else {
