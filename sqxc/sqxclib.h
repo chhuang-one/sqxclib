694fb7e6d6a257553db4c3b3a13e5310421bd012
C.H. Huang <plushuang.tw@gmail.com>
2021-05-15 08:51:18 +0800

    add missing file for previous commit.

---
+++ sqxc/sqxclib.h
@@ -0,0 +1,31 @@
+/*
+ *   Copyright (C) 2021 by C.H. Huang
+ *   plushuang.tw@gmail.com
+ *
+ * sqxc is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+#include <SqConfig.h>
+#include <SqDefine.h>
+#include <SqError.h>
+
+#include <SqUtil.h>
+#include <SqStorage.h>
+#include <SqQuery.h>
+#include <SqJoint.h>
+
+#include <SqdbSqlite.h>
+#include <SqdbMysql.h>
+
+#include <SqxcValue.h>
+#include <SqxcJsonc.h>
+#include <SqxcSql.h>
+
+#include <SqType-stl-cpp.h>    // Sq::TypeStl<StlContainer>
