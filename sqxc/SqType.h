f5021e78a037c4bc6e4103545d555fc2aca8ecce
C.H. Huang <plushuang.tw@gmail.com>
2021-09-21 08:52:00 +0800

    SqType can be derived by using Sq::TypeMethod and SQ_TYPE_MEMBERS
    SqType must have no base struct because I need use aggregate initialization with it.
    If you add, remove, or change methods in Sq::TypeMethod, do the same things in SqType.

--- sqxc/SqType.h
+++ sqxc/SqType.h
@@ -183,15 +183,92 @@ Sqxc *sq_type_object_write(void *instance, const SqType *type, Sqxc *xc_dest);
 #endif
 
 // ----------------------------------------------------------------------------
+// C++ declarations: declare C++ data, function, method, and others.
+
+#ifdef __cplusplus
+
+namespace Sq {
+
+/*	TypeMethod : C++ struct is used by SqType and it's children.
+
+	Note: If you add, remove, or change methods here, do the same things in SqType.
+ */
+
+struct TypeMethod {
+	// these function only work if SqType.bit_field has SQB_TYPE_DYNAMIC
+	void  ref();
+	void  unref();
+
+	// create dynamic SqType and copy data from static SqType
+	SqType *copyStatic(SqDestroyFunc entry_free_func);
+
+	// initialize/finalize self
+	void  initSelf(int prealloc_size, SqDestroyFunc entry_destroy_func);
+	void  initSelf(int prealloc_size, void (*entry_destroy_func)(SqEntry*) );
+	void  finalSelf();
+
+	// initialize/finalize instance
+	void *initInstance(void *instance, int is_pointer = 0);
+	void  finalInstance(void *instance, int is_pointer = 0);
+
+	// add entry from SqEntry array (NOT pointer array) to dynamic SqType.
+	// if 'sizeof_entry' == 0, 'sizeof_entry' will equal sizeof(SqEntry)
+	void  addEntry(const SqEntry *entry, int n_entry = 1, size_t sizeof_entry = 0);
+
+	// add entry from SqEntry pointer array to dynamic SqType.
+	void  addEntry(const SqEntry **entry_ptrs, int n_entry_ptrs = 1);
+
+	// find SqEntry in SqType.entry.
+	// If cmp_func is NULL and SqType.entry is sorted, it will use binary search to find entry by name.
+	SqEntry **findEntry(const void *key, SqCompareFunc cmp_func = NULL);
+
+	// sort SqType.entry by name if SqType is dynamic.
+	void  sortEntry();
+
+	// calculate size for dynamic SqType.
+	// if "inner_entry" == NULL, it use all entries to calculate size.
+	// otherwise it use "inner_entry" to calculate size.
+	unsigned int  decideSize(const SqEntry *inner_entry = NULL, bool entry_removed = false);
+
+	// erase entry in SqType if SqType is dynamic.
+	void     eraseEntry(void **element_addr, int count = 1);
+	void     eraseEntry(SqEntry **element_addr, int count = 1);
+
+	// steal entry in SqType if SqType is dynamic.
+	void     stealEntry(void **element_addr, int count = 1);
+	void     stealEntry(SqEntry **element_addr, int count = 1);
+};
+
+};  // namespace Sq
+
+#endif  // __cplusplus
+
+// ----------------------------------------------------------------------------
 // C/C++ common definitions: define structue
 
 /*
-    SqType - define how to initialize, finalize, and convert instance.
-             User can define constant or dynamic SqType.
+	SqType - define how to initialize, finalize, and convert instance.
+	         User can define constant or dynamic SqType.
+
+	SqType must have no base struct because I need use aggregate initialization with it.
  */
+
+#define SQ_TYPE_MEMBERS           \
+	unsigned int      size;       \
+	SqTypeFunc        init;       \
+	SqTypeFunc        final;      \
+	SqTypeParseFunc   parse;      \
+	SqTypeWriteFunc   write;      \
+	char             *name;       \
+	SqEntry         **entry;      \
+	int               n_entry;    \
+	uint16_t          bit_field;  \
+	uint16_t          ref_count
 
 struct SqType
 {
+//	SQ_TYPE_MEMBERS;
+/*	// ------ SqType members ------  */
 	unsigned int   size;        // instance size
 
 	SqTypeFunc     init;        // initialize instance
@@ -218,6 +295,8 @@ struct SqType
 	uint16_t       ref_count;    // reference count for dynamic SqType only
 
 #ifdef __cplusplus
+	/* Note: If you add, remove, or change methods here, do the same things in Sq::TypeMethod. */
+
 	// these function only work if SqType.bit_field has SQB_TYPE_DYNAMIC
 	void  ref() {
 		sq_type_ref((SqType*)this);
@@ -275,18 +354,6 @@ struct SqType
 	unsigned int  decideSize(const SqEntry *inner_entry = NULL, bool entry_removed = false) {
 		return sq_type_decide_size((SqType*)this, inner_entry, entry_removed);
 	}
-
-	/*
-	// sample code for eraseEntry() and stealEntry()
-
-	SqEntry** entryAddr;
-
-	entryAddr = type->findEntry("field_name");
-	if (entryAddr) {
-		type->eraseEntry(entryAddr);
-//		type->stealEntry(entryAddr);
-	}
-	 */
 
 	// erase entry in SqType if SqType is dynamic.
 	void     eraseEntry(void **element_addr, int count = 1) {
@@ -464,6 +531,91 @@ extern  const  SqType      SqType_StdString_;    // C++ std::string
  */
 
 namespace Sq {
+
+// these function only work if SqType.bit_field has SQB_TYPE_DYNAMIC
+inline void  TypeMethod::ref() {
+	sq_type_ref((SqType*)this);
+}
+inline void  TypeMethod::unref() {
+	sq_type_unref((SqType*)this);
+}
+
+// create dynamic SqType and copy data from static SqType
+inline SqType *TypeMethod::copyStatic(SqDestroyFunc entry_free_func) {
+	return sq_type_copy_static((const SqType*)this, entry_free_func);
+}
+
+// initialize/finalize self
+inline void  TypeMethod::initSelf(int prealloc_size, SqDestroyFunc entry_destroy_func) {
+	sq_type_init_self((SqType*)this, prealloc_size, entry_destroy_func);
+}
+inline void  TypeMethod::initSelf(int prealloc_size, void (*entry_destroy_func)(SqEntry*) ) {
+	sq_type_init_self((SqType*)this, prealloc_size, (SqDestroyFunc)entry_destroy_func);
+}
+inline void  TypeMethod::finalSelf() {
+	sq_type_final_self((SqType*)this);
+}
+
+// initialize/finalize instance
+inline void *TypeMethod::initInstance(void *instance, int is_pointer) {
+	return sq_type_init_instance((const SqType*)this, instance, is_pointer);
+}
+inline void  TypeMethod::finalInstance(void *instance, int is_pointer) {
+	sq_type_final_instance((const SqType*)this, instance, is_pointer);
+}
+
+// add entry from SqEntry array (NOT pointer array) to dynamic SqType.
+// if 'sizeof_entry' == 0, 'sizeof_entry' will equal sizeof(SqEntry)
+inline void  TypeMethod::addEntry(const SqEntry *entry, int n_entry, size_t sizeof_entry) {
+	sq_type_add_entry((SqType*)this, entry, n_entry, sizeof_entry);
+}
+// add entry from SqEntry pointer array to dynamic SqType.
+inline void  TypeMethod::addEntry(const SqEntry **entry_ptrs, int n_entry_ptrs) {
+	sq_type_add_entry_ptrs((SqType*)this, entry_ptrs, n_entry_ptrs);
+}
+
+// find SqEntry in SqType.entry.
+// If cmp_func is NULL and SqType.entry is sorted, it will use binary search to find entry by name.
+inline SqEntry **TypeMethod::findEntry(const void *key, SqCompareFunc cmp_func) {
+	return (SqEntry**)sq_type_find_entry((const SqType*)this, key, cmp_func);
+}
+// sort SqType.entry by name if SqType is dynamic.
+inline void  TypeMethod::sortEntry() {
+	sq_type_sort_entry((SqType*)this);
+}
+// calculate size for dynamic SqType.
+// if "inner_entry" == NULL, it use all entries to calculate size.
+// otherwise it use "inner_entry" to calculate size.
+inline unsigned int  TypeMethod::decideSize(const SqEntry *inner_entry, bool entry_removed) {
+	return sq_type_decide_size((SqType*)this, inner_entry, entry_removed);
+}
+
+// erase entry in SqType if SqType is dynamic.
+inline void     TypeMethod::eraseEntry(void **element_addr, int count) {
+	SQ_TYPE_ERASE_ENTRY_ADDR((SqType*)this, element_addr, count);
+}
+inline void     TypeMethod::eraseEntry(SqEntry **element_addr, int count) {
+	SQ_TYPE_ERASE_ENTRY_ADDR((SqType*)this, element_addr, count);
+}
+// steal entry in SqType if SqType is dynamic.
+inline void     TypeMethod::stealEntry(void **element_addr, int count) {
+	SQ_TYPE_STEAL_ENTRY_ADDR((SqType*)this, element_addr, count);
+}
+inline void     TypeMethod::stealEntry(SqEntry **element_addr, int count) {
+	SQ_TYPE_STEAL_ENTRY_ADDR((SqType*)this, element_addr, count);
+}
+
+/*
+// sample code for eraseEntry() and stealEntry()
+
+	SqEntry** entryAddr;
+
+	entryAddr = type->findEntry("field_name");
+	if (entryAddr) {
+		type->eraseEntry(entryAddr);
+//		type->stealEntry(entryAddr);
+	}
+ */
 
 /* --- define C++11 standard-layout structures --- */
 typedef struct SqType    Type;
