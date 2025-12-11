34d63f382f97d515fafc6509b1b3904a33f92285
C.H. Huang <plushuang.tw@gmail.com>
2025-12-11 08:29:05 +0800

    add sqxcapp.h, it include all headers in sqxcapp.

---
+++ sqxcapp/sqxcapp.h
@@ -0,0 +1,22 @@
+/*
+ *   Copyright (C) 2022-2025 by C.H. Huang
+ *   plushuang.tw@gmail.com
+ *
+ * sqxclib is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+// SqAppTool.h will include headers in sqxcsupport.
+
+#include <SqApp.h>
+#include <SqAppTool.h>
+#include <SqMigration.h>
+
+#include <SqAppOptions.h>
+#include <SqAppCommands.h>
